55d31f79252d78593c3852454df14822e62cd596
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-05-20 15:50:01 +0530

    implement orderBy conversion

--- src/orderBy.ts
+++ src/orderBy.ts
@@ -1,4 +1,14 @@
-export const makeOrderByPrisma2Compatible = (input: any): any => {
-  console.log('working');
-  return true;
+type OrderType = 'asc' | 'desc';
+
+export const makeOrderByPrisma2Compatible = (
+  input: string
+): Record<string, OrderType> => {
+  const index = input.lastIndexOf('_');
+  const [field, order] = [
+    input.slice(0, index),
+    input.slice(index + 1).toLocaleLowerCase() as OrderType,
+  ];
+  return {
+    [field]: order,
+  };
 };
--- test/orderBy.test.ts
+++ test/orderBy.test.ts
@@ -1,5 +1,25 @@
 import { makeOrderByPrisma2Compatible } from '../src/orderBy';
 
-it('works', () => {
-  expect(makeOrderByPrisma2Compatible({})).toBe(true);
+it('runs on basic cases', () => {
+  expect(makeOrderByPrisma2Compatible('id_ASC')).toStrictEqual({
+    id: 'asc',
+  });
+  expect(makeOrderByPrisma2Compatible('id_DESC')).toStrictEqual({
+    id: 'desc',
+  });
+  expect(makeOrderByPrisma2Compatible('published_ASC')).toStrictEqual({
+    published: 'asc',
+  });
+  expect(makeOrderByPrisma2Compatible('published_DESC')).toStrictEqual({
+    published: 'desc',
+  });
+});
+
+it('runs on underscored fields', () => {
+  expect(makeOrderByPrisma2Compatible('first_name_ASC')).toStrictEqual({
+    first_name: 'asc',
+  });
+  expect(makeOrderByPrisma2Compatible('total_price_DESC')).toStrictEqual({
+    total_price: 'desc',
+  });
 });
